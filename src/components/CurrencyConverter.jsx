c3643da9cfc59e0f8927c390e711016679171230
DEBADATTAJENA <debadattajena25@gmail.com>
2024-05-15 01:04:52 +0530

    first commit for currency convertor

---
+++ src/components/CurrencyConverter.jsx
@@ -0,0 +1,118 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import CurrencyDropdown from "./Dropdown";
+import { HiArrowsRightLeft } from "react-icons/hi2";
+
+const CurrencyConverter = () => {
+  const [currencies, setCurrencies] = useState([]);
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [convertedAmount, setConvertedAmount] = useState(null);
+  const [converting, setConverting] = useState(false);
+
+  const fetchCurrencies = async () => {
+    try {
+      const res = await fetch("https://api.frankfurter.app/currencies");
+      const data = await res.json();
+      setCurrencies(Object.keys(data));
+    } catch (error) {
+      console.error("Error Fetching", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCurrencies();
+  }, []);
+
+  //   console.log(currencies);
+  let tenCurrencies = currencies;
+
+  const convertCurrency = async () => {
+    if (!amount) return;
+    setConverting(true);
+    try {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+      );
+      const data = await res.json();
+
+      setConvertedAmount(data.rates[toCurrency] + " " + toCurrency);
+    } catch (error) {
+      console.error("Error Fetching", error);
+    } finally {
+      setConverting(false);
+    }
+  };
+
+  console.log("isConverting", converting);
+  const swapCurrencies = () => {
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
+  };
+
+  return (
+    <div className="max-w-xl mx-auto my-10 p-5 bg-white rounded-lg shadow-md">
+      <h2 className="flex justify-center items-center mb-6 text-2xl font-bold underline bg-gradient-to-t from-blue-800 to-yellow-600 bg-clip-text text-transparent">
+        Currency Converter
+      </h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 items-end">
+        <CurrencyDropdown
+          currencies={tenCurrencies}
+          title="From:"
+          currency={fromCurrency}
+          setCurrency={setFromCurrency}
+        />
+        {/* swap currency button */}
+        <div className="flex justify-center -mb-5 sm:mb-0">
+          <button
+            onClick={swapCurrencies}
+            className="p-2 bg-gray-200 rounded-full cursor-pointer hover:bg-gray-300"
+          >
+            <HiArrowsRightLeft className="text-xl text-gray-700" />
+          </button>
+        </div>
+
+        <CurrencyDropdown
+          currencies={tenCurrencies}
+          currency={toCurrency}
+          setCurrency={setToCurrency}
+          title="To:"
+        />
+      </div>
+
+      <div className="mt-4">
+        <label
+          htmlFor="amount"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Amount:
+        </label>
+        <input
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          type="number"
+          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-1"
+        />
+      </div>
+
+      <div className="flex justify-end mt-6">
+        <button
+          onClick={convertCurrency}
+          className="px-5 py-2 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          {converting ? "Converting..." : "Convert"}
+        </button>
+      </div>
+
+      {convertedAmount && (
+        <div className="flex justify-center items-center my-4 text-lg font-medium text-right text-green-600">
+          Converted Amount: {convertedAmount}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CurrencyConverter;
---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+// import { HiOutlineStar, HiStar } from "react-icons/hi2";
+
+const CurrencyDropdown = ({
+  currencies,
+  currency,
+  setCurrency,
+  favorites,
+  //   handleFavorite,
+  title = "",
+}) => {
+  //   const isFavorite = (curr) => favorites.includes(curr);
+
+  return (
+    <div>
+      <label
+        htmlFor={title}
+        className="block text-sm font-medium text-gray-700"
+      >
+        {title}
+      </label>
+
+      <div className="mt-1 relative">
+        <select
+          value={currency}
+          onChange={(e) => setCurrency(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        >
+          {currencies?.map((currency) => {
+            return (
+              <option value={currency} key={currency}>
+                {currency}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default CurrencyDropdown;
